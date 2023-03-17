7685fad25cc266e53800a145f17381b836ea9cf9
insirenator <arsshakeeb149@protonmail.com>
2023-03-17 17:25:24 +0530

    Final Version

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-function flipColor() {
+let flipBtn = document.getElementById('flip-btn');
+let color_area = document.getElementById("clr-area");
+let color_name = document.getElementById("clr-name");
+let simpleMode = document.getElementById('simple-mode');
+let hexMode = document.getElementById('hex-mode');
 
-	let color_area = document.getElementById("clr-area");
-	let color_name = document.getElementById("clr-name");
+flipBtn.addEventListener('click', flipSimpleColor);
+
+simpleMode.addEventListener('click', () =>{
+	flipBtn.removeEventListener('click', flipHexColor);
+	flipBtn.addEventListener('click', flipSimpleColor);
+	color_area.style.background = 'White';
+	color_name.textContent = 'White';
+});
+
+hexMode.addEventListener('click', () =>{
+	flipBtn.removeEventListener('click', flipSimpleColor);
+	flipBtn.addEventListener('click', flipHexColor);
+	color_area.style.background = '#FFFFFF';
+	color_name.textContent = '#FFFFFF';
+});
+
+function flipHexColor() {
 	
-	let color = getRandomColor();
+	let color = getRandomHexColor();
 	color_area.style.background = color;
 	color_name.textContent = color;
 }
 
-function getRandomColor() {
+function getRandomHexColor() {
 	let color = '#';
 	let hex = '0123456789ABCDEF';
 
@@ -19,4 +38,21 @@ function getRandomColor() {
 	}
 
 	return color;
+}
+
+function flipSimpleColor() {
+
+	let color = getRandomSimpleColor();
+	color_area.style.background = color;
+	color_name.textContent = color;
+}
+
+function getRandomSimpleColor() {
+	let colors = ['Red', 'White', 'Blue', 'Green',
+					'Purple', 'Yellow', 'Cyan', 'Aqua',
+					'Maroon', 'Teal', 'Coral', 'Olive'];
+
+	let idx = Math.floor(Math.random()*colors.length);
+
+	return colors[idx];
 }
